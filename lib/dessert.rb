b636908a5782191357cacd0b6a2170f90be86f2a
Preston <powens1@citadel.edu>
2016-09-21 17:36:20 +0000

    update

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,32 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
+  
   def healthy?
-    # your code here
+    @calories < 200
   end
+  
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
-  def initialize(flavor)
-    # your code here
+  attr_accessor :flavor
+  def initialize (flavor)
+    @flavor = flavor
+    @calories = 5
+    @name = flavor + " jelly bean"
   end
+  def delicious?
+    if flavor == "licorice"
+      return false
+    else
+      return true
+    end
+  end
+  
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,41 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+	  s = self.gsub(/[^a-zA-Z]+/, "")
+	  s.downcase!
+
+	  if s == s.reverse
+	  	return true
+	  else
+	  	return false
+	  end
   end
+
+  
   def count_words
-    # your code here
+	    word_count = Hash.new(0)
+	    mod_s = self.gsub(/[^a-z ]/i, '')
+	    mod_s.downcase!
+	    mod_s.split.each do |x|
+		  	word_count[x] += 1
+	    end
+	    return word_count
   end
+    
   def anagram_groups
-    # your code here
+    if self.empty?
+    	return Array.new
+    end
+    string_array = self.split
+    group = Hash.new
+    string_array.each do |word|
+    	word_sorted = word.downcase.chars.sort
+    	if group.has_key?(word_sorted)
+    		group.fetch(word_sorted).insert(0,word)
+    	else
+    		group[word_sorted] = [word]
+    	end
+    end
+    return group.values
   end
 end
 
