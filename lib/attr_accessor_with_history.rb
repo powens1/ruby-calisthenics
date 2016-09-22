8bb879dae4c5b7a07c6d0b430f7cb7355006d8e0
Preston <powens1@citadel.edu>
2016-09-22 23:12:01 +0000

    update

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -1,12 +1,17 @@
 class Class
-  attr_accessor :attr_name
-  
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
-    @attr_reader = attr_name # create the attribute's getter
+    attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(attr_name)
+        if @#{attr_name}_history == nil
+          @#{attr_name}_history = [nil]
+        else
+          @#{attr_name}_history << @#{attr_name}
+        end
+        @#{attr_name}=(attr_name)
+      end
     }
   end
 end
